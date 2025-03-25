7e84862743df0316bb72fd6c9d5538708dd2e5ec
simran singh <simran27@bu.edu>
2025-03-25 17:59:04 -0400

    ui changed main font and background

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Home: React.FC = () => {
     return (
-        <>
+        <div style= {{ color: '#6A0DAD'}} >
             <h1>Home Page</h1>
             <p>Welcome to SparkBites! Our goal is to connect the BU Community
                 with on-campus events that are trying to eliminate food waste</p>
-        </>
+        </div>
     );
 };
 
