a977be8f1e9f30b21347227b9aa168611fb0174e
simran singh <simran27@bu.edu>
2025-03-25 18:07:56 -0400

    updated font color

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Contact: React.FC = () => {
     return (
-        <>
+        <div style= {{ color: '#6A0DAD'}}>
             <h1>Contact Page</h1>
             <p>Contact us at <strong>sparkbites@bu.edu</strong></p>
-        </>
+        </div>
     );
 };
 
