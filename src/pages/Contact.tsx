9450c25d3bf65d0a69430e0c68e93290fce71437
simran singh <simran27@bu.edu>
2025-03-25 18:15:37 -0400

    added roboto mono font

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -4,7 +4,9 @@ const Contact: React.FC = () => {
     return (
         <div style= {{ color: '#6A0DAD'}}>
             <h1>Contact Page</h1>
+            <div style={{fontFamily: "'Roboto Mono', monospace"}}>
             <p>Contact us at <strong>sparkbites@bu.edu</strong></p>
+            </div>
         </div>
     );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,9 +3,14 @@ import React from 'react';
 const Home: React.FC = () => {
     return (
         <div style= {{ color: '#6A0DAD'}} >
+            <div>
             <h1>Home Page</h1>
+            </div>
+            <div style={{fontFamily: "'Roboto Mono', monospace"}}>
             <p>Welcome to SparkBites! Our goal is to connect the BU Community
-                with on-campus events that are trying to eliminate food waste</p>
+            with on-campus events that are trying to eliminate food waste</p>
+            </div>
+
         </div>
     );
 };
